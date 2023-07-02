65b6b04eb7823ae18c52c82f8356f21b54cfe2b2
EMMANUEL08161823021 <117604958+EMMANUEL08161823021@users.noreply.github.com>
2023-07-03 00:27:47 +0100

    Add files via upload

---
+++ Web-designs/details.js
@@ -0,0 +1,21 @@
+const slider = document.getElementById('slider');
+// import mySlider from '../Web-designs/details.json'
+const mySlider =  [
+    {
+        name: 'Real Estate',
+        texts: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+        // image:
+    },
+    {
+        name: 'Word Cross',
+        texts: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+    },
+    {
+        name: 'Puzzle',
+        texts: '',
+    },
+]
+
+
+
+
